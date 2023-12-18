9293d870fac35b2a9216b0f6c42c67db58fbc6d3
menjaraz <menjaraz@gmail.com>
2023-12-18 23:46:44 +0300

    update tetration

--- tetration/source/app.d
+++ tetration/source/app.d
@@ -1,6 +1,28 @@
+import std.stdio: writefln;
+import std.bigint : BigInt;
+import std.conv: to;
+
+BigInt tetration(int base, int height)
+{
+    if (height == 0)
+        return 1.BigInt;
+
+    BigInt result = to!BigInt(base);
+
+    for (int i = 1; i < height; ++i)
+    {
+        result = result ^^ base;
+    }
+
+    return result;
+}
+
 void main()
 {
-    import std.stdio, std.bigint;
+    const auto BASE = 2;
+    const auto HEIGHT = 16;
 
-    writeln(9_999.BigInt^^999);
+    BigInt result = tetration(BASE, HEIGHT);
+
+    writefln("\nTetration of %d to the power of %d is: %s", BASE, HEIGHT, result);
 }
