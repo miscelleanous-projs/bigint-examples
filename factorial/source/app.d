ff347bb94fe0ca3a504a96487557c2599161c015
menjaraz <menjaraz@gmail.com>
2023-12-19 00:14:17 +0300

    Add factorial

---
+++ factorial/source/app.d
@@ -0,0 +1,21 @@
+import std.stdio;
+import std.BigInt;
+import std.range;
+import std.algorithm;
+import std.conv;
+
+BigInt factorial(uint n) pure
+{
+    auto b = 1.BigInt;
+	
+	foreach(i; iota(1, n + 1))
+		b *= i;
+	
+	return b;
+}
+
+
+void main()
+{
+	writefln("\n%(%s\\\n%)", 999.factorial.text.chunks(80));
+}
