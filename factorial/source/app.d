6a244892c1256676c9ac4eaf094c316be3ad1715
menjaraz <menjaraz@gmail.com>
2023-12-19 00:17:55 +0300

    update README.md

--- factorial/source/app.d
+++ factorial/source/app.d
@@ -7,15 +7,15 @@ import std.conv;
 BigInt factorial(uint n) pure
 {
     auto b = 1.BigInt;
-	
-	foreach(i; iota(1, n + 1))
-		b *= i;
-	
-	return b;
+    
+    foreach(i; iota(1, n + 1))
+        b *= i;
+    
+    return b;
 }
 
 
 void main()
 {
-	writefln("\n%(%s\\\n%)", 999.factorial.text.chunks(80));
+    writefln("\n%(%s\\\n%)", 999.factorial.text.chunks(80));
 }
