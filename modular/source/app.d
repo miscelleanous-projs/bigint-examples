54a485d77300170fecc559afd9256de88a0b766f
Menjanahary <menjaraz@gmail.com>
2023-12-19 12:33:37 +0400

    Update app.d

--- modular/source/app.d
+++ modular/source/app.d
@@ -1,6 +1,6 @@
 void main()
 {
-    import std.stdio, std.bigint;
+    import std.stdio, std.bigint, std.conv, std.range;
 
-    writeln(powmod(9_999.BigInt, 999.BigInt, 10.BigInt^^1_00));
+    writefln("\n%(%s\\\n%)", (powmod(9_999.BigInt, 999.BigInt, 10.BigInt^^1_00)).text.chunks(80));
 }
